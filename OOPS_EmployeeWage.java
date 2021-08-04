04718ab2db2fb2e21834076356f0d3cd191d411c
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-08-04 15:30:22 +0530

    Adding Daily wage

--- OOPS_EmployeeWage.java
+++ OOPS_EmployeeWage.java
@@ -3,6 +3,8 @@ package Day8;
 import java.util.Random;
 
 public class OOPS_EmployeeWage {
+	
+	static int WAGE_PER_HOUR= 200;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -13,4 +15,9 @@ public class OOPS_EmployeeWage {
 		Random ran = new Random();
 		int isPresent= ran.nextInt(2);
 	}
+	
+	public static int dailyWage(int h) {
+		return h*WAGE_PER_HOUR;
+	}
+	
 }
