37ec2a6b51a46e4bc50bf5f91728dbf9a71c6dc2
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-08-04 15:39:58 +0530

    adding part time

--- OOPS_EmployeeWage.java
+++ OOPS_EmployeeWage.java
@@ -7,13 +7,12 @@ public class OOPS_EmployeeWage {
 	static int WAGE_PER_HOUR= 200;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Computation Program");
 	}
 
 	public static void isPresent() {
 		Random ran = new Random();
-		int isPresent= ran.nextInt(2);
+		int isPresent= ran.nextInt(3);
 	}
 	
 	public static int dailyWage(int h) {
