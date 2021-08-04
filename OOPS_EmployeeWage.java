eba40bf8983d73507de8be923da87be3dbb85abc
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-08-04 15:47:26 +0530

    using switch

--- OOPS_EmployeeWage.java
+++ OOPS_EmployeeWage.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class OOPS_EmployeeWage {
 	
 	static int WAGE_PER_HOUR= 200;
+	static int hours;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
@@ -13,6 +14,11 @@ public class OOPS_EmployeeWage {
 	public static void isPresent() {
 		Random ran = new Random();
 		int isPresent= ran.nextInt(3);
+		switch(isPresent) {
+			case 0: hours=0; break;
+			case 1: hours=4; break;
+			case 2: hours=8; break;
+		}
 	}
 	
 	public static int dailyWage(int h) {
